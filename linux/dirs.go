2fe98f1c96d9f7fc148e1e75975a3eb890af3320
Boris Rybalkin <ribalkin@gmail.com>
2024-07-28 18:03:10 +0100

    simplify mkdirall

--- linux/dirs.go
+++ linux/dirs.go
@@ -6,18 +6,7 @@ import (
 
 func CreateMissingDirs(dirs ...string) error {
 	for _, dir := range dirs {
-		err := CreateMissingDir(dir)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func CreateMissingDir(dir string) error {
-	_, err := os.Stat(dir)
-	if os.IsNotExist(err) {
-		err = os.Mkdir(dir, 0755)
+		err := os.MkdirAll(dir, 0755)
 		if err != nil {
 			return err
 		}
